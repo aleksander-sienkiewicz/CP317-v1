ee585415f7bb04f408c622b4f763a28e37a161ef
Rocchi12 <94766910+Rocchi12@users.noreply.github.com>
2023-07-06 14:44:10 -0400

    Update main.py

--- textdissasember/main.py
+++ textdissasember/main.py
@@ -25,20 +25,49 @@ class articleDissasembler:
         return(pages)
     def extractTables(self):
         #get images and text in each page
-
+        page = []
         tablepage = []
 
         #gets tables in each page
         for i in (range(1,(self.length+1))):
             stri = str(i)
             table = tabula.read_pdf(self.fileName, pages=stri, multiple_tables=True, stream='true')
-  
-            tablepage.append(table[0].to_numpy())
+
+            for i in table:
+                page.append(i.to_records())
+            tablepage.append(page)
+            page = []
 
         return(tablepage)
+    def getEquations(self):
+        a = self.extractTextAndImages()
+        temp = ""
+        counter = 0
+        line = 0
+        for i in range(0,len(a[4])-1):
+            if (a[4][i] != '\n'):
+                counter +=1
+            else:
+                print(counter, line)
+                line += 1
+                counter = 0
+        # page = self.file.new_page()
+        # text = '\xe2\x8e\xaa\xe2\x8e\xa9\n\xe2\x8e\xaa\xe2\x8e\xa8\n\xe2\x8e\xa7\n\xe2\x89\xa0\n\xe2\x88\x92\n=\n= \xe2\x88\x91\n\xe2\x89\xa0\nj\n'
+        # p = fitz.Point(50, 72) 
+
+        # rc = page.insert_text(p,  # bottom-left of 1st char
+        #                     text,  # the text (honors '\n')
+        #                     fontname = "helv",  # the default font
+        #                     fontsize = 11,  # the default font size
+        #                     rotate = 0,  # also available: 90, 180, 270
+        #                     )
+        # print("%i lines printed on page %i." % (rc, page.number))
+
+        # self.file.save("output2.pdf")
+
          
 
-a = articleDissasembler("test4.pdf")
+a = articleDissasembler("tablesample.pdf")
 b = a.extractTables()
 
 print(b)
